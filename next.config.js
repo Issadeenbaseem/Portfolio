fbf18fc4ae24c3ab0c96a83f1fba5a0a8f445b7c
Issadeenbaseem <issadeenbaseem95@gmail.com>
2024-09-10 18:49:04 +0400

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+module.exports = {
   output: 'export',
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-
+  assetPrefix: './',
   images: {
     unoptimized: true,
   },
-}
-
-module.exports = nextConfig
+};
